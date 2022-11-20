8905c525b97078bc7e1e90d070de4cf3e0595438
charat <charatkosit@gmail.com>
2022-11-20 22:59:16 +0700

    new4

--- aws-back-sit/script/deploy-back-sit.sh
+++ aws-back-sit/script/deploy-back-sit.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 #stop container before
 docker stop $(docker ps --filter 'ancestor=charat/go-back-sit:latest' --format "{{.ID}}")
-cd /home/ec2-user/docker-mysql-pma/
-docker-compose down --rmi all
-cd /home/ec2-user/
-
-#update LineToken 
-#AWS-Front:
-tokenLineAF=K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG
-#AWS-Back:
-tokenLineAB=ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz
-
-cat >> ~/.bashrc << EOF
-export tokenLineAF=K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG
-export tokenLineAB=ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz
-EOF
 
 #remove image <none> and  go-back
 docker images
--- update-script-back-prod.sh
+++ update-script-back-prod.sh
@@ -1,6 +1,25 @@
 !#/bin/bash
 git clone https://github.com/charatkosit/go-aws-script.git temp 
 
+#stop docker-mysql-pma
+cd /home/ec2-user/docker-mysql-pma/
+docker-compose down --rmi all
+cd /home/ec2-user/
+
+#update LineToken 
+#AWS-Front:
+tokenLineAF=K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG
+#AWS-Back:
+tokenLineAB=ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz
+
+cat >> ~/.bashrc << EOF
+export tokenLineAF=K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG
+export tokenLineAB=ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz
+EOF
+
+#copy /data to rest
+mkdir data
+cp /home/ec2-user/docker-mysql-pma/data/*.*  /home/ec2-user/data
 #remove 
 rm -rf docker-mysql-pma
 rm -rf script
@@ -8,6 +27,12 @@ rm -rf script
 #make dir
 mkdir script
 mkdir docker-mysql-pma
+mkdir docker-mysql-pma/data
+
+#/data to /docker-mysql-pwa
+cp /home/ec2-user/data/*.* /home/ec2-user/docker-mysql-pma/data
+rm -rf /home/ec2-user/data
+
 
 #copy new
 cp /home/ec2-user/temp/aws-back-prod/script/*.* /home/ec2-user/script/
