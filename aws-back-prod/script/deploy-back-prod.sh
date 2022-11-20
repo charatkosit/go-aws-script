45e96fc5319dc822248acdbd2fc642753d5aa943
charat <charatkosit@gmail.com>
2022-11-20 22:45:58 +0700

    new3

--- aws-back-prod/script/deploy-back-prod.sh
+++ aws-back-prod/script/deploy-back-prod.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 #stop container before
 docker stop $(docker ps --filter 'ancestor=charat/go-back-prod:latest' --format "{{.ID}}")
+cd /home/ec2-user/docker-mysql-pma/
+docker-compose down --rmi all
+cd /home/ec2-user/
+
+#update LineToken 
+#AWS-Front:
+tokenLineAF=K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG
+#AWS-Back:
+tokenLineAB=ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz
+
+cat >> ~/.bashrc << EOF
+export tokenLineAF=K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG
+export tokenLineAB=ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz
+EOF
+
 #remove image <none> and  go-back
 docker images
 docker rmi $(docker images go-back-prod --format "{{.ID}}") -f
@@ -10,4 +25,4 @@ docker images
 #pull and run
 docker pull charat/go-back-prod:latest
 docker run -p 3000:3000 -d charat/go-back-prod:latest
-curl -X POST -H "Authorization: Bearer ${tokenLine}" -F "message=PROD new version http://goapi.ddns.net:3000/api/v1" https://notify-api.line.me/api/notify
+curl -X POST -H "Authorization: Bearer ${tokenLineAB}" -F "message=PROD new version http://goapi.ddns.net:3000/api/v1" https://notify-api.line.me/api/notify
