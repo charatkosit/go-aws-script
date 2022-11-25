2a4b15f73c36d263420f6b33eb5e469d25bda3e2
charat <charatkosit@gmail.com>
2022-11-25 19:00:53 +0700

    new update-deploy

--- aws-back-uat/script/deploy-back-uat.sh
+++ aws-back-uat/script/deploy-back-uat.sh
@@ -12,6 +12,10 @@ docker images
 docker pull charat/go-back-uat:latest
 docker run -p 3000:3000 -d charat/go-back-uat:latest
 
+#wait 5 sec
+sleep 5 &
+wait
+
 #get version form api to notify
 latestVer=`curl http://54.224.78.26:3000/api/v1/env |cut -d '"' -f 52`
 curl -X POST -H "Authorization: Bearer ${tokenLineAB}" -F "message=UAT deploy latest Version: ${latestVer} OK" https://notify-api.line.me/api/notify
