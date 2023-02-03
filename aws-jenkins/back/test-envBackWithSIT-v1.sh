5d19b4446022d4d9be28af071cdad5866be751df
charat <charatkosit@gmail.com>
2023-02-03 21:54:20 +0700

    add test-back

---
+++ aws-jenkins/back/test-envBackWithSIT-v1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#-------
+#cd /var/lib/jenkins/workspace/test-back-sit/src/environments/
+#rm -rf environment.ts
+#cp environment.sit.ts environment.ts
+#cd /home/ec2-user
+#-------
+
+tagnameBS=`cat /var/lib/jenkins/workspace/test-back-sit/package.json |grep "version" |cut -d '"' -f 4`
+releaseBS=`echo $tagnameBS |cut -d ' ' -f 1`
+
+sed -i -e "s|127.0.0.1|172.41.60.173|g" /var/lib/jenkins/workspace/test-back-sit/src/environments/environment.ts
+sed -i -e "s|%release%|${releaseBS}|g" /var/lib/jenkins/workspace/test-back-sit/src/environments/environment.ts
+#debug
+cat /var/lib/jenkins/workspace/test-back-sit/src/environments/environment.ts
+
+curl -X POST -H "Authorization: Bearer ${tokenLineB}" -F "message=SIT prepare Code ${releaseBS} OK" https://notify-api.line.me/api/notify
