9e653ac82bebc204d90d16808ebb13c643a2dbc5
charat <charatkosit@gmail.com>
2023-02-06 11:58:32 +0700

    up23

--- aws-jenkins/front/envFrontWithSIT.sh
+++ aws-jenkins/front/envFrontWithSIT.sh
@@ -3,7 +3,7 @@
 tagname=`cat /var/lib/jenkins/workspace/go-front-sit/package.json |grep "version" |cut -d '"' -f 4`
 releaseFS=`echo $tagname |cut -d ' ' -f 1`
 
-backendUrl='http://172.41.61.11:3000/'
+backendUrl='http://172.41.53.205:3000/'
 authUrl='http://172.41.60.18:3000/'
 sapUrl='http://192.168.20.17:8880/apigoplus/EnqPartlist/'
 sapApiToken='z@hz3sNY#0ohB9SspeE9@fLDQ%r65x$k8LxL28VH72FfvRWgCn'
