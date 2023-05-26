321f3bf68b3c9162a81b2b26bd5bd44aac7b853f
charat <charatkosit@gmail.com>
2023-05-26 23:14:54 +0700

    26052314

--- aws-jenkins/front/newEnvFrontWithUAT.sh
+++ aws-jenkins/front/newEnvFrontWithUAT.sh
@@ -3,8 +3,8 @@
 tagname=`cat /var/lib/jenkins/workspace/go-front-uat/package.json |grep "version" |cut -d '"' -f 4`
 releaseFS=`echo $tagname |cut -d ' ' -f 1`
 
-backendUrl='http://172.51.66.169:3000/'
-authUrl='http://172.51.66.169:3000/'
+backendUrl='http://172.51.66.196:3000/'
+authUrl='http://172.51.66.196:3000/'
 sapUrl='http://192.168.20.17:8880/apigoplus/EnqPartlist/'
 sapApiToken='z@hz3sNY#0ohB9SspeE9@fLDQ%r65x$k8LxL28VH72FfvRWgCn'
 stage='UAT' 
