52d95c51868d3f5719808473d6018439a82fe33d
charat <charatkosit@gmail.com>
2023-07-05 09:51:35 +0700

    update 572566951

--- aws-jenkins/back/envAuthWithPRD-v1.sh
+++ aws-jenkins/back/envAuthWithPRD-v1.sh
@@ -6,12 +6,12 @@
 # cd /home/ec2-user
 #--------
 
-tagnameBP=`cat /var/lib/jenkins/workspace/go-auth-prod/package.json |grep "version" |cut -d '"' -f 4`
+tagnameBP=`cat /var/lib/jenkins/workspace/go-back-auth-prod/package.json |grep "version" |cut -d '"' -f 4`
 releaseBP=`echo $tagnameBP |cut -d ' ' -f 1`
 
-sed -i -e "s|127.0.0.1|172.51.67.165|g" /var/lib/jenkins/workspace/go-auth-prod/src/environments/environment.ts
-sed -i -e "s|%release%|${releaseBP}|g" /var/lib/jenkins/workspace/go-auth-prod/src/environments/environment.ts
+sed -i -e "s|127.0.0.1|172.51.67.165|g" /var/lib/jenkins/workspace/go-back-auth-prod/src/environments/environment.ts
+sed -i -e "s|%release%|${releaseBP}|g" /var/lib/jenkins/workspace/go-back-auth-prod/src/environments/environment.ts
 #debug
-cat /var/lib/jenkins/workspace/go-auth-prod/src/environments/environment.ts
+cat /var/lib/jenkins/workspace/go-back-auth-prod/src/environments/environment.ts
 
 curl -X POST -H "Authorization: Bearer ${tokenLineB}" -F "message=PROD prepare Code ${releaseBP} OK" https://notify-api.line.me/api/notify
