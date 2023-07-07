b8537ab71304a0889b86d0325906c249c549e885
charat <charatkosit@gmail.com>
2023-07-07 14:38:08 +0700

    update 7725661438

--- aws-jenkins/front/dockerPush-front-prod.sh
+++ aws-jenkins/front/dockerPush-front-prod.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-releaseFS=`docker images go-front-sit --format {{.Tag}} | head -1`
+releaseFS=`docker images go-front-prod --format {{.Tag}} | head -1`
 
-docker image tag go-front-sit:$releaseFS go-front-sit:latest
-docker image tag go-front-sit:$releaseFS charat/go-front-sit:$releaseFS 
-docker image tag go-front-sit:latest charat/go-front-sit:latest
-docker image push --all-tags charat/go-front-sit
+docker image tag go-front-prod:$releaseFS go-front-prod:latest
+docker image tag go-front-prod:$releaseFS charat/go-front-prod:$releaseFS 
+docker image tag go-front-prod:latest charat/go-front-prod:latest
+docker image push --all-tags charat/go-front-prod
 
 docker images
-docker rmi $(docker images go-front-sit --format "{{.ID}}") -f
+docker rmi $(docker images go-front-prod --format "{{.ID}}") -f
 docker rmi -f $(docker images --format "{{.ID}}" --filter "dangling=true")
 docker images
 
-curl -X POST -H "Authorization: Bearer ${tokenLineF}" -F "message=SIT push DockerHub  ${releaseFS} OK" https://notify-api.line.me/api/notify
+curl -X POST -H "Authorization: Bearer ${tokenLineF}" -F "message=PROD push DockerHub  ${releaseFS} OK" https://notify-api.line.me/api/notify
