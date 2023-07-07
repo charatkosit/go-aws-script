b80116cca209c62a65112a2961ba7f08e2b32b43
charat <charatkosit@gmail.com>
2023-07-08 00:07:38 +0700

    update 872566007

--- aws-jenkins/back/envAuthWithUAT-v1.sh
+++ aws-jenkins/back/envAuthWithUAT-v1.sh
@@ -9,7 +9,7 @@
 tagnameBU=`cat /var/lib/jenkins/workspace/go-auth-uat/package.json |grep "version" |cut -d '"' -f 4`
 releaseBU=`echo $tagnameBU |cut -d ' ' -f 1`
 
-sed -i -e "s|127.0.0.1|172.41.60.78|g" /var/lib/jenkins/workspace/go-auth-uat/src/environments/environment.ts
+sed -i -e "s|127.0.0.1|172.51.64.139|g" /var/lib/jenkins/workspace/go-auth-uat/src/environments/environment.ts
 sed -i -e "s|%release%|${releaseBU}|g" /var/lib/jenkins/workspace/go-auth-uat/src/environments/environment.ts
 #debug
 cat /var/lib/jenkins/workspace/go-auth-uat/src/environments/environment.ts
--- aws-jenkins/back/envBackWithPRD-v1.sh
+++ aws-jenkins/back/envBackWithPRD-v1.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #--------
-# cd /var/lib/jenkins/workspace/go-auth-prod/src/environments/
+# cd /var/lib/jenkins/workspace/go-back-prod/src/environments/
 # rm -rf environment.ts
 # cp environment.prod.ts environment.ts
 # cd /home/ec2-user
 #--------
 
-tagnameBP=`cat /var/lib/jenkins/workspace/go-auth-prod/package.json |grep "version" |cut -d '"' -f 4`
+tagnameBP=`cat /var/lib/jenkins/workspace/go-back-prod/package.json |grep "version" |cut -d '"' -f 4`
 releaseBP=`echo $tagnameBP |cut -d ' ' -f 1`
 
-sed -i -e "s|127.0.0.1|172.51.64.167|g" /var/lib/jenkins/workspace/go-auth-prod/src/environments/environment.ts
-sed -i -e "s|%release%|${releaseBP}|g" /var/lib/jenkins/workspace/go-auth-prod/src/environments/environment.ts
+sed -i -e "s|127.0.0.1|172.51.66.196|g" /var/lib/jenkins/workspace/go-back-prod/src/environments/environment.ts
+sed -i -e "s|%release%|${releaseBP}|g" /var/lib/jenkins/workspace/go-back-prod/src/environments/environment.ts
 #debug
-cat /var/lib/jenkins/workspace/go-auth-prod/src/environments/environment.ts
+cat /var/lib/jenkins/workspace/go-back-prod/src/environments/environment.ts
 
 curl -X POST -H "Authorization: Bearer ${tokenLineB}" -F "message=PROD prepare Code ${releaseBP} OK" https://notify-api.line.me/api/notify
