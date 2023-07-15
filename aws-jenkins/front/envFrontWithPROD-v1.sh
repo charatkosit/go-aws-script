9c9a64281ee6c82ab336c068a615a04c9067d5e7
charatkosit <charatkosit@gmail.com>
2023-07-15 22:20:38 +0700

    1572566-2220

--- aws-jenkins/front/envFrontWithPROD-v1.sh
+++ aws-jenkins/front/envFrontWithPROD-v1.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #prepare  data
+#
+# Env for Go-Back-PROD
+# ver 0.0.2 15/7/66 22:19
 tagname=`cat /var/lib/jenkins/workspace/go-front-prod/package.json |grep "version" |cut -d '"' -f 4`
 releaseFS=`echo $tagname |cut -d ' ' -f 1`
 
 backendUrl='http://172.51.66.196:3000/'
 authUrl='http://172.41.60.18:3000/'
-sapUrl='http://192.168.20.17:8880/apigoplus/EnqPartlist/'
-sapApiToken='z@hz3sNY#0ohB9SspeE9@fLDQ%r65x$k8LxL28VH72FfvRWgCn'
 stage='PROD' 
 timestamp=$(date)
 
 #replace data
 sed -i -e "s|%backendUrl%|${backendUrl}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
 sed -i -e "s|%authUrl%|${authUrl}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
-sed -i -e "s|%sapUrl%|${sapUrl}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
-sed -i -e "s|%sapApiToken%|${sapApiToken}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
+# sed -i -e "s|%sapUrl%|${sapUrl}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
+# sed -i -e "s|%sapApiToken%|${sapApiToken}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
 sed -i -e "s|%stage%|${stage}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
 sed -i -e "s|%timestamp%|${timestamp}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
 sed -i -e "s|%release%|${releaseFS}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
--- aws-jenkins/front/envFrontWithUAT-v1.sh
+++ aws-jenkins/front/envFrontWithUAT-v1.sh
@@ -2,23 +2,21 @@
 #prepare  data
 #
 # Env for Go-Back-UAT
-# ver 0.0.2 15/7/66 21:55
+# ver 0.0.3 15/7/66 22:19
 
 tagname=`cat /var/lib/jenkins/workspace/go-front-uat/package.json |grep "version" |cut -d '"' -f 4`
 releaseFS=`echo $tagname |cut -d ' ' -f 1`
 
 backendUrl='http://172.51.66.196:3000/'
 authUrl='http://172.51.66.196:3000/'
-sapUrl='http://192.168.20.17:8880/apigoplus/EnqPartlist/'
-sapApiToken='z@hz3sNY#0ohB9SspeE9@fLDQ%r65x$k8LxL28VH72FfvRWgCn'
 stage='UAT' 
 timestamp=$(date)
 
 #replace data
 sed -i -e "s|%backendUrl%|${backendUrl}|g" /var/lib/jenkins/workspace/go-front-uat/src/environments/environment.prod.ts
 sed -i -e "s|%authUrl%|${authUrl}|g" /var/lib/jenkins/workspace/go-front-uat/src/environments/environment.prod.ts
-sed -i -e "s|%sapUrl%|${sapUrl}|g" /var/lib/jenkins/workspace/go-front-uat/src/environments/environment.prod.ts
-sed -i -e "s|%sapApiToken%|${sapApiToken}|g" /var/lib/jenkins/workspace/go-front-uat/src/environments/environment.prod.ts
+# sed -i -e "s|%sapUrl%|${sapUrl}|g" /var/lib/jenkins/workspace/go-front-uat/src/environments/environment.prod.ts
+# sed -i -e "s|%sapApiToken%|${sapApiToken}|g" /var/lib/jenkins/workspace/go-front-uat/src/environments/environment.prod.ts
 sed -i -e "s|%stage%|${stage}|g" /var/lib/jenkins/workspace/go-front-uat/src/environments/environment.prod.ts
 sed -i -e "s|%timestamp%|${timestamp}|g" /var/lib/jenkins/workspace/go-front-uat/src/environments/environment.prod.ts
 sed -i -e "s|%release%|${releaseFS}|g" /var/lib/jenkins/workspace/go-front-uat/src/environments/environment.prod.ts
