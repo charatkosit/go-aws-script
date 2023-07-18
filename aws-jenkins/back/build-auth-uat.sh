daf935f069bac0f61e1df214aeeb211fb5adfd14
charatkosit <charatkosit@gmail.com>
2023-07-18 17:34:57 +0700

    1872566-1734

--- aws-jenkins/back/build-auth-uat.sh
+++ aws-jenkins/back/build-auth-uat.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "hello"
-tagnameBU=`cat /var/lib/jenkins/workspace/go-auth-uat/package.json |grep "version" |cut -d'"' -f 4`
+echo "build-auth-uat.sh"
+tagnameBU=`cat /var/lib/jenkins/workspace/Go-Auth-UAT/package.json |grep "version" |cut -d'"' -f 4`
 releaseBU=`echo $tagnameBU |cut -d ' ' -f 1`
 echo $releaseBU
 
-sed -i -e "s/go-auth-xxx/go-auth-uat/g" /var/lib/jenkins/workspace/go-auth-uat/docker-compose.yml
-sed -i -e "s/latest/${releaseBU}/g" /var/lib/jenkins/workspace/go-auth-uat/docker-compose.yml
-cat /var/lib/jenkins/workspace/go-auth-uat/docker-compose.yml
+sed -i -e "s/go-auth-xxx/go-auth-uat/g" /var/lib/jenkins/workspace/Go-Auth-UAT/docker-compose.yml
+sed -i -e "s/latest/${releaseBU}/g" /var/lib/jenkins/workspace/Go-Auth-UAT/docker-compose.yml
+cat /var/lib/jenkins/workspace/Go-Auth-UAT/docker-compose.yml
 
-cd  /var/lib/jenkins/workspace/go-auth-uat/
+cd  /var/lib/jenkins/workspace/Go-Auth-UAT/
 docker-compose build prod
 cd /home/ec2-user/
 
