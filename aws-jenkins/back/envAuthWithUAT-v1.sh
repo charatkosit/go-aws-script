9d6ccaef380bf2f74f15e25c2446377f68dba54d
charatkosit <charatkosit@gmail.com>
2023-07-18 17:32:05 +0700

    1872566-1732

--- aws-jenkins/back/envAuthWithUAT-v1.sh
+++ aws-jenkins/back/envAuthWithUAT-v1.sh
@@ -6,12 +6,12 @@
 # cd /home/ec2-user
 #------
 
-tagnameBU=`cat /var/lib/jenkins/workspace/go-auth-uat/package.json |grep "version" |cut -d '"' -f 4`
+tagnameBU=`cat /var/lib/jenkins/workspace/Go-Auth-UAT/package.json |grep "version" |cut -d '"' -f 4`
 releaseBU=`echo $tagnameBU |cut -d ' ' -f 1`
 
-sed -i -e "s|127.0.0.1|172.51.64.139|g" /var/lib/jenkins/workspace/go-auth-uat/src/environments/environment.ts
-sed -i -e "s|Back-Auth-Dev|${releaseBU}|g" /var/lib/jenkins/workspace/go-auth-uat/src/environments/environment.ts
+sed -i -e "s|127.0.0.1|172.51.64.139|g" /var/lib/jenkins/workspace/Go-Auth-UAT/src/environments/environment.ts
+sed -i -e "s|Back-Auth-Dev|${releaseBU}|g" /var/lib/jenkins/workspace/Go-Auth-UAT/src/environments/environment.ts
 #debug
-cat /var/lib/jenkins/workspace/go-auth-uat/src/environments/environment.ts
+cat /var/lib/jenkins/workspace/Go-Auth-UAT/src/environments/environment.ts
 
 curl -X POST -H "Authorization: Bearer ${tokenLineB}" -F "message=UAT Build Code ${releaseBU} OK" https://notify-api.line.me/api/notify
