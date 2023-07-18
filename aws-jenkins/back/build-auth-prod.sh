4fdd945c57f0d99806a01edca4d2ecccf3431a3d
charatkosit <charatkosit@gmail.com>
2023-07-18 19:22:38 +0700

    1872566-1922

--- aws-jenkins/back/build-auth-prod.sh
+++ aws-jenkins/back/build-auth-prod.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "hello"
-tagnameBP=`cat /var/lib/jenkins/workspace/go-auth-prod/package.json |grep "version" |cut -d'"' -f 4`
+tagnameBP=`cat /var/lib/jenkins/workspace/Go-Auth-PROD/package.json |grep "version" |cut -d'"' -f 4`
 releaseBP=`echo $tagnameBP |cut -d ' ' -f 1`
 echo $releaseBP
 
-sed -i -e "s/go-auth-xxx/go-auth-prod/g" /var/lib/jenkins/workspace/go-auth-prod/docker-compose.yml
-sed -i -e "s/latest/${releaseBP}/g" /var/lib/jenkins/workspace/go-auth-prod/docker-compose.yml
+sed -i -e "s/go-auth-xxx/go-auth-prod/g" /var/lib/jenkins/workspace/Go-Auth-PROD/docker-compose.yml
+sed -i -e "s/latest/${releaseBP}/g" /var/lib/jenkins/workspace/Go-Auth-PROD/docker-compose.yml
 
-cat /var/lib/jenkins/workspace/go-auth-prod/docker-compose.yml
+cat /var/lib/jenkins/workspace/Go-Auth-PROD/docker-compose.yml
 
-cd  /var/lib/jenkins/workspace/go-auth-prod/
+cd  /var/lib/jenkins/workspace/Go-Auth-PROD/
 docker-compose build prod
 cd /home/ec2-user/
 
--- aws-jenkins/back/dockerPush-auth-prod.sh
+++ aws-jenkins/back/dockerPush-auth-prod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-releasePBP=`docker images go-back-auth-prod --format {{.Tag}} | head -1`
+releasePBP=`docker images go-auth-prod --format {{.Tag}} | head -1`
 
-docker image tag go-back-auth-prod:$releasePBP go-back-auth-prod:latest
-docker image tag go-back-auth-prod:$releasePBP charat/go-back-auth-prod:$releasePBP 
-docker image tag go-back-auth-prod:latest charat/go-back-auth-prod:latest
-docker image push --all-tags charat/go-back-auth-prod
+docker image tag go-auth-prod:$releasePBP go-auth-prod:latest
+docker image tag go-auth-prod:$releasePBP charat/go-auth-prod:$releasePBP 
+docker image tag go-auth-prod:latest charat/go-auth-prod:latest
+docker image push --all-tags charat/go-auth-prod
 
 docker images
-docker rmi $(docker images go-back-auth-prod --format "{{.ID}}") -f
+docker rmi $(docker images go-auth-prod --format "{{.ID}}") -f
 #docker rmi -f $(docker images --format "{{.ID}}" --filter "dangling=true")
 docker images
 
