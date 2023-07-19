2c5591c0daf80f1893b44ee650b3fdf0c2868ce9
charatkosit <charatkosit@gmail.com>
2023-07-19 09:36:15 +0700

    1972566-936

---
+++ update-deploy-auth-prod.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+git clone https://github.com/charatkosit/go-aws-script.git temp 
+
+cp /home/ec2-user/temp/aws-auth-prod/script/deploy-auth-prod.sh  /home/ec2-user/script/
+
+rm -rf /home/ec2-user/temp
---
+++ update-deploy-auth-sit.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+git clone https://github.com/charatkosit/go-aws-script.git temp 
+
+cp /home/ec2-user/temp/aws-auth-sit/script/deploy-auth-sit.sh  /home/ec2-user/script/
+
+rm -rf /home/ec2-user/temp
