659d4140af22fe13d470c4b4ba6f04cb601e1648
charatkosit <charatkosit@gmail.com>
2023-07-19 21:03:31 +0700

    1972566-2103

--- aws-auth-uat/script/stopAll-auth-uat.sh
+++ aws-auth-uat/script/stopAll-auth-uat.sh
@@ -9,7 +9,14 @@ if docker ps -q >/dev/null 2>&1; then
     docker rm $(docker ps -aqf "status=exited")
 
     # ลบ images images go-auth-uat
-    docker rmi $(docker images go-auth-uat --format "{{.ID}}") -f
+ 
+    # ตรวจสอบว่ามี image ชื่อ charat/go-auth-uat หรือไม่
+    if docker image ls | grep -q charat/go-auth-uat; then
+         # ลบ image ชื่อ charat/go-auth-uat
+         docker rmi charat/go-auth-uat
+    else
+         echo "Image charat/go-auth-uat not found. Skipping..."
+    fi
 else
     # หากไม่มี container ทำงานอยู่
     echo "All containers are stopped"
