c8190e0e134bb32a8e4e4805a4bbcf2039a6eed5
charatkosit <charatkosit@gmail.com>
2023-07-19 21:12:09 +0700

    1972566-2112

--- aws-auth-uat/script/startAll-auth-uat.sh
+++ aws-auth-uat/script/startAll-auth-uat.sh
@@ -6,7 +6,15 @@ docker-compose up -d
 cd /home/ec2-user/
 
 #สตาร์ท go-auth-uat
-cd /home/ec2-user/script
+docker pull charat/go-auth-uat:latest
 docker run -p 3100:3100 -d charat/go-auth-uat:latest
+
+#wait 5 sec
+sleep 5 &
+wait
+
+#get version form api to notify
+latestVer=`curl http://172.51.64.139:3100/api/v1/env |cut -d '"' -f 52`
+curl -X POST -H "Authorization: Bearer ${tokenLineAB}" -F "message=UAT deploy latest Version: ${latestVer} OK" https://notify-api.line.me/api/notify
 
 
