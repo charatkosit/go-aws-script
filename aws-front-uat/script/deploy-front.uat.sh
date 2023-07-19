6cf35303be3b050b25f8b8eee5fe6478c51b119d
charatkosit <charatkosit@gmail.com>
2023-07-19 19:47:16 +0700

    1972566-1947

--- aws-front-uat/script/deploy-front.uat.sh
+++ aws-front-uat/script/deploy-front.uat.sh
@@ -4,7 +4,7 @@ docker rmi $(docker images --format "{{.ID}}" --filter "dangling=true" | head -1
 docker stop $(docker ps --filter "ancestor=charat/go-front-uat:latest" -q)
 docker rm $(docker ps --filter "ancestor=charat/go-front-uat:latest" -aq)
 docker pull charat/go-front-uat:latest
-docker run -p 8000:80 -d charat/go-front-uat:latest --restart=always
+docker run -p 8000:80 -d charat/go-front-uat:latest
 curl -X POST -H "Authorization: Bearer ${tokenLineAF}" -F "message=UAT deploy new version http://gouat.ddns.net:8000" https://notify-api.line.me/api/notify
 
 #stat containter when system restart
