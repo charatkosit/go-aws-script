4e4b876370cddcd8d25cdaef0667542a075cb912
charatkosit <charatkosit@gmail.com>
2023-07-19 11:49:48 +0700

    1972566-1149

--- aws-front-sit/script/deploy-front-sit.sh
+++ aws-front-sit/script/deploy-front-sit.sh
@@ -4,7 +4,7 @@ docker rmi $(docker images --format "{{.ID}}" --filter "dangling=true" | head -1
 docker stop $(docker ps --filter "ancestor=charat/go-front-sit:latest" -q)
 docker rm $(docker ps --filter "ancestor=charat/go-front-sit:latest" -aq)
 docker pull charat/go-front-sit:latest
-docker run -p 8000:80 -d charat/go-front-sit:latest
+docker run -p 8000:80 -d charat/go-front-sit:latest --restart=always
 curl -X POST -H "Authorization: Bearer ${tokenLineAF}" -F "message=SIT deploy new version http://gosit.ddns.net:8000" https://notify-api.line.me/api/notify
 
 #stat containter when system restart
