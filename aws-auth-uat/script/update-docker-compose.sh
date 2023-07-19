c9c27ddc7ca0f3fc6702f15b945ac5c0a15bb348
charatkosit <charatkosit@gmail.com>
2023-07-19 22:02:24 +0700

    1972566-2202

---
+++ aws-auth-uat/script/update-docker-compose.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/charatkosit/go-aws-script.git temp
+cp /home/ec2-user/temp/aws-auth-uat/docker-mysql-pma/*.yml /home/ec2-user/docker-mysql-pma/
+
+#remove temp
+cd /home/ec2-user/
+rm -rf temp
