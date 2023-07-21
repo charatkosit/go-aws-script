0f0ef9941aa553e0da7d72d40de6c78ce07e075d
charatkosit <charatkosit@gmail.com>
2023-07-21 14:24:28 +0700

    2172566-1424

--- aws-jenkins/back/build-auth-sit.sh
+++ aws-jenkins/back/build-auth-sit.sh
@@ -4,12 +4,12 @@ tagnameBS=`cat /var/lib/jenkins/workspace/go-auth-sit/package.json |grep "versio
 releaseBS=`echo $tagnameBS |cut -d ' ' -f 1`
 echo $releaseBS
 
-sed -i -e "s/go-auth-xxx/go-auth-sit/g" /var/lib/jenkins/workspace/go-auth-sit/docker-compose.yml
-sed -i -e "s/latest/${releaseBS}/g" /var/lib/jenkins/workspace/go-auth-sit/docker-compose.yml
-cat /var/lib/jenkins/workspace/go-auth-sit/docker-compose.yml
+# sed -i -e "s/go-auth-xxx/go-auth-sit/g" /var/lib/jenkins/workspace/go-auth-sit/docker-compose.yml
+# sed -i -e "s/latest/${releaseBS}/g" /var/lib/jenkins/workspace/go-auth-sit/docker-compose.yml
+# cat /var/lib/jenkins/workspace/go-auth-sit/docker-compose.yml
 
 cd  /var/lib/jenkins/workspace/go-auth-sit/
-docker-compose build prod
+# docker-compose build prod
 cd /home/ec2-user/
 
 curl -X POST -H "Authorization: Bearer ${tokenLineB}" -F "message=SIT Build Code ${releaseBS} OK" https://notify-api.line.me/api/notify
--- aws-jenkins/back/envAuthWithSIT-v1.sh
+++ aws-jenkins/back/envAuthWithSIT-v1.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-#-------
-#cd /var/lib/jenkins/workspace/go-auth-sit/src/environments/
-#rm -rf environment.ts
-#cp environment.sit.ts environment.ts
-#cd /home/ec2-user
-#-------
+
 
 tagnameBS=`cat /var/lib/jenkins/workspace/go-auth-sit/package.json |grep "version" |cut -d '"' -f 4`
 releaseBS=`echo $tagnameBS |cut -d ' ' -f 1`
 
-sed -i -e "s|127.0.0.1|172.41.60.78|g" /var/lib/jenkins/workspace/go-auth-sit/src/environments/environment.ts
+sed -i -e "s|127.0.0.1|172.51.66.79|g" /var/lib/jenkins/workspace/go-auth-sit/src/environments/environment.ts
 sed -i -e "s|%release%|${releaseBS}|g" /var/lib/jenkins/workspace/go-auth-sit/src/environments/environment.ts
 #debug
 cat /var/lib/jenkins/workspace/go-auth-sit/src/environments/environment.ts
