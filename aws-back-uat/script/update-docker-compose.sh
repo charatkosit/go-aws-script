d902c831bf8df0be7ddde3050895b9a043d6376c
charatkosit <charatkosit@gmail.com>
2023-07-22 01:10:13 +0700

    2272566-0109

---
+++ aws-back-uat/script/update-docker-compose.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /home/ec2-user/
+git clone https://github.com/charatkosit/go-aws-script.git temp
+cp /home/ec2-user/temp/aws-back-uat/docker-mysql-pma/*.yml /home/ec2-user/docker-mysql-pma/
+
+#remove temp
+cd /home/ec2-user/
+rm -rf temp
+cd /home/ec2-user/script/
