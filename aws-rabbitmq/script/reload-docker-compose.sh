d8eb7d13aac315679fb895935810ba52ab04f213
charatkosit <charatkosit@gmail.com>
2023-07-22 16:05:02 +0700

    2272566-1604

--- aws-rabbitmq/script/reload-docker-compose.sh
+++ aws-rabbitmq/script/reload-docker-compose.sh
@@ -2,9 +2,11 @@
 rm -rf  /home/ec2-user/temp
 rm -rf /home/ec2-user/docker-rabbitmq
 
+cd /home/ec2-user/
 git clone https://github.com/charatkosit/go-aws-script.git temp
 
 mkdir /home/ec2-user/docker-rabbitmq
 
 cp /home/ec2-user/temp/aws-rabbitmq/docker-compose.yml /home/ec2-user/docker-rabbitmq
 
+rm -rf  /home/ec2-user/temp
