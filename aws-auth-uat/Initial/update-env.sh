ce5a7b5757e8dbfd847a380db7200034d0cef45d
charatkosit <charatkosit@gmail.com>
2023-07-28 22:23:50 +0700

    2872566-2223

---
+++ aws-auth-uat/Initial/update-env.sh
@@ -0,0 +1,14 @@
+!#/bin/bash
+
+#set environment
+cat >> ~/.bashrc << EOF
+export tokenLineAF=K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG
+export tokenLineAB=ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz
+export tokenLineDeployGroup=nS2OKcPZ9lXNb03JHJ7xzl4YNDBdzswkTg1qK8iNDQD
+export UTC_OFFSET=7
+export dbUsername=root
+export dbPassword=1234
+export dbName=testhaha
+export sapApiToken=z@hz3sNY#0ohB9SspeE9@fLDQ%r65x$k8LxL28VH72FfvRWgCn
+export jwt_secrect=!z$C&F)J@NcRfUjXn2r5u8x/A?D*G-Ka
+EOF
