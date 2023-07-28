f490614d4e289a953f31afc57055957dd437e734
charatkosit <charatkosit@gmail.com>
2023-07-28 23:29:51 +0700

    2872566-2329

--- aws-back-prod/Initial/update-env.sh
+++ aws-back-prod/Initial/update-env.sh
@@ -3,8 +3,5 @@
 #set environment
 
 cat >> ~/.bashrc << EOF
-export tokenLineAF='K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG'
-export tokenLineAB='ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz'
-export tokenLineDeployGroup='nS2OKcPZ9lXNb03JHJ7xzl4YNDBdzswkTg1qK8iNDQD'
 export UTC_OFFSET=7
 EOF
