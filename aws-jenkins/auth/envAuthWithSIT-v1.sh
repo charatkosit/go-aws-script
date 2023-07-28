b156a19155de6389a2050b7790b48215837b848d
charatkosit <charatkosit@gmail.com>
2023-07-28 13:20:35 +0700

    2872566-1320

--- aws-jenkins/auth/envAuthWithSIT-v1.sh
+++ aws-jenkins/auth/envAuthWithSIT-v1.sh
@@ -4,7 +4,7 @@
 tagnameBS=`cat /var/lib/jenkins/workspace/Go-Auth-SIT/package.json |grep "version" |cut -d '"' -f 4`
 releaseBS=`echo $tagnameBS |cut -d ' ' -f 1`
 
-sed -i -e "s|127.0.0.1|172.51.66.79|g" /var/lib/jenkins/workspace/Go-Auth-SIT/src/environments/environment.ts
+sed -i -e "s|127.0.0.1|172.51.48.57|g" /var/lib/jenkins/workspace/Go-Auth-SIT/src/environments/environment.ts
 sed -i -e "s|Back-Auth-Dev|${releaseBS}|g" /var/lib/jenkins/workspace/Go-Auth-SIT/src/environments/environment.ts
 #debug
 cat /var/lib/jenkins/workspace/Go-Auth-SIT/src/environments/environment.ts
