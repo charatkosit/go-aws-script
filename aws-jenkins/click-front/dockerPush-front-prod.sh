62ba4266f5121f129e03607ae513ca93147f6333
charatkosit <charatkosit@gmail.com>
2023-09-05 11:30:05 +0700

    update click-front-prod

--- aws-jenkins/click-front/dockerPush-front-prod.sh
+++ aws-jenkins/click-front/dockerPush-front-prod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-releaseFP=`docker images go-front-prod --format {{.Tag}} | head -1`
+releaseFP=`docker images click-front-prod --format {{.Tag}} | head -1`
 
-docker image tag go-front-prod:$releaseFP go-front-prod:latest
-docker image tag go-front-prod:$releaseFP charat/go-front-prod:$releaseFP
-docker image tag go-front-prod:latest charat/go-front-prod:latest
-docker image push --all-tags charat/go-front-prod
+docker image tag click-front-prod:$releaseFP click-front-prod:latest
+docker image tag click-front-prod:$releaseFP charat/click-front-prod:$releaseFP
+docker image tag click-front-prod:latest charat/click-front-prod:latest
+docker image push --all-tags charat/click-front-prod
 
 docker images
-docker rmi $(docker images go-front-prod --format "{{.ID}}") -f
+docker rmi $(docker images click-front-prod --format "{{.ID}}") -f
 docker rmi -f $(docker images --format "{{.ID}}" --filter "dangling=true")
 docker images
 
--- aws-jenkins/click-front/envFrontWithPRD.sh
+++ aws-jenkins/click-front/envFrontWithPRD.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #prepare  data
-tagname=`cat /var/lib/jenkins/workspace/go-front-prod/package.json |grep "version" |cut -d'"' -f 4`
+tagname=`cat /var/lib/jenkins/workspace/click-front-prod/package.json |grep "version" |cut -d'"' -f 4`
 releaseFP=`echo $tagname |cut -d ' ' -f 1 `
 
-backendUrl='http://goapi.ddns.net:3200/'
+backendUrl='http://clickapi.ddns.net:3200/'
 authUrl='http://172.41.60.18:3000'
 sapUrl=''
 sapApiToken=''
@@ -11,17 +11,17 @@ stage='PROD'
 timestamp=$(date)
 
 #replace data
-sed -i -e "s|%backendUrl%|${backendUrl}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
-sed -i -e "s|%authUrl%|${authUrl}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
-sed -i -e "s|%sapUrl%|${sapUrl}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
-sed -i -e "s|%sapApiToken%|${sapApiToken}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
-sed -i -e "s|%stage%|${stage}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
-sed -i -e "s|%timestamp%|${timestamp}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
-sed -i -e "s|%release%|${releaseFP}|g" /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
+sed -i -e "s|%backendUrl%|${backendUrl}|g" /var/lib/jenkins/workspace/click-front-prod/src/environments/environment.prod.ts
+sed -i -e "s|%authUrl%|${authUrl}|g" /var/lib/jenkins/workspace/click-front-prod/src/environments/environment.prod.ts
+sed -i -e "s|%sapUrl%|${sapUrl}|g" /var/lib/jenkins/workspace/click-front-prod/src/environments/environment.prod.ts
+sed -i -e "s|%sapApiToken%|${sapApiToken}|g" /var/lib/jenkins/workspace/click-front-prod/src/environments/environment.prod.ts
+sed -i -e "s|%stage%|${stage}|g" /var/lib/jenkins/workspace/click-front-prod/src/environments/environment.prod.ts
+sed -i -e "s|%timestamp%|${timestamp}|g" /var/lib/jenkins/workspace/click-front-prod/src/environments/environment.prod.ts
+sed -i -e "s|%release%|${releaseFP}|g" /var/lib/jenkins/workspace/click-front-prod/src/environments/environment.prod.ts
 #replace data proxy in nginx
-sed -i -e "s|172.41.62.59:3000|172.41.62.164:3000|g" /var/lib/jenkins/workspace/go-front-uat/nginx.conf
+sed -i -e "s|172.41.62.59:3000|172.41.62.164:3000|g" /var/lib/jenkins/workspace/click-front-prod/nginx.conf
 #debug
-cat /var/lib/jenkins/workspace/go-front-prod/src/environments/environment.prod.ts
+cat /var/lib/jenkins/workspace/click-front-prod/src/environments/environment.prod.ts
 
-curl -X POST -H "Authorization: Bearer ${tokenLineJenkinClick}" -F "message=Click-Front-UAT prepare Code ${releaseFP} OK" https://notify-api.line.me/api/notify
+curl -X POST -H "Authorization: Bearer ${tokenLineJenkinClick}" -F "message=Click-Front-PROD prepare Code ${releaseFP} OK" https://notify-api.line.me/api/notify
 
