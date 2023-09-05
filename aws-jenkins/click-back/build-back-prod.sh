362650b051476d0716053f16f670845c71ccf202
charatkosit <charatkosit@gmail.com>
2023-09-05 17:33:16 +0700

    1733

--- aws-jenkins/click-back/build-back-prod.sh
+++ aws-jenkins/click-back/build-back-prod.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 echo "hello"
-tagnameBS=`cat /var/lib/jenkins/workspace/Click-Prod-Back/package.json |grep "version" |cut -d'"' -f 4`
+tagnameBS=`cat /var/lib/jenkins/workspace/Click-Back-Prod/package.json |grep "version" |cut -d'"' -f 4`
 releaseBS=`echo $tagnameBS |cut -d ' ' -f 1`
 echo $releaseBS
 
-# sed -i -e "s/click-back-xxx/Click-Prod-Back/g" /var/lib/jenkins/workspace/Click-Prod-Back/docker-compose.yml
-# sed -i -e "s/latest/${releaseBS}/g" /var/lib/jenkins/workspace/Click-Prod-Back/docker-compose.yml
-# cat /var/lib/jenkins/workspace/Click-Prod-Back/docker-compose.yml
+# sed -i -e "s/click-back-xxx/Click-Back-Prod/g" /var/lib/jenkins/workspace/Click-Back-Prod/docker-compose.yml
+# sed -i -e "s/latest/${releaseBS}/g" /var/lib/jenkins/workspace/Click-Back-Prod/docker-compose.yml
+# cat /var/lib/jenkins/workspace/Click-Back-Prod/docker-compose.yml
 
-cd  /var/lib/jenkins/workspace/Click-Prod-Back/
-docker-compose build prod
+cd /var/lib/jenkins/workspace/Click-Back-Prod/
+docker build -t click-front-prod:$releaseFP -f ./Dockerfile .
 cd /home/ec2-user/
-
 curl -X POST -H "Authorization: Bearer ${tokenLineJenkinClick}" -F "message=Click-Back-PROD Build Code ${releaseBS} OK" https://notify-api.line.me/api/notify
