5b0e5b771a196f063f5254883faaac4598286c11
charatkosit <charatkosit@gmail.com>
2023-09-05 17:23:51 +0700

    1723

--- aws-jenkins/click-back/envBackWithPROD-v1.sh
+++ aws-jenkins/click-back/envBackWithPROD-v1.sh
@@ -10,9 +10,9 @@ tagnameBS=`cat /var/lib/jenkins/workspace/Click-Back-Prod/package.json |grep "ve
 releaseBS=`echo $tagnameBS |cut -d ' ' -f 1`
 
 # 172.31.24.223 is click.All.Back.SIT
-sed -i -e "s|127.0.0.1|172.31.24.223|g" /var/lib/jenkins/workspace/Click-Back-Prod/src/environments/environment.ts
-sed -i -e "s|%release%|${releaseBS}|g" /var/lib/jenkins/workspace/Click-Back-Prod/src/environments/environment.ts
+# sed -i -e "s|127.0.0.1|172.31.24.223|g" /var/lib/jenkins/workspace/Click-Back-Prod/src/environments/environment.ts
+# sed -i -e "s|%release%|${releaseBS}|g" /var/lib/jenkins/workspace/Click-Back-Prod/src/environments/environment.ts
 #debug
-cat /var/lib/jenkins/workspace/Click-Back-Prod/src/environments/environment.ts
+# cat /var/lib/jenkins/workspace/Click-Back-Prod/src/environments/environment.ts
 
 curl -X POST -H "Authorization: Bearer ${tokenLineJenkinClick}" -F "message=Click-Back-PROD prepare Code ${releaseBS} OK" https://notify-api.line.me/api/notify
