43bb038121841b2cc83a8b576a4fea8de6f8d9a1
charatkosit <charatkosit@gmail.com>
2023-09-07 14:07:22 +0700

    up 1407

--- aws-jenkins/test/build-log-event.sh
+++ aws-jenkins/test/build-log-event.sh
@@ -4,8 +4,8 @@ tagnameBP=`cat /var/lib/jenkins/workspace/test.LogEvent/package.json |grep "vers
 releaseBP=`echo $tagnameBP |cut -d ' ' -f 1`
 echo $releaseBP
 
-sed -i -e "s/go-back-yyy/test.LogEvent/g" /var/lib/jenkins/workspace/test.LogEvent/docker-compose.yml
-sed -i -e "s/go-back-xxx/test.LogEvent/g" /var/lib/jenkins/workspace/test.LogEvent/docker-compose.yml
+sed -i -e "s/go-back-yyy/logevent/g" /var/lib/jenkins/workspace/test.LogEvent/docker-compose.yml
+sed -i -e "s/go-back-xxx/logevent/g" /var/lib/jenkins/workspace/test.LogEvent/docker-compose.yml
 sed -i -e "s/latest/${releaseBP}/g" /var/lib/jenkins/workspace/test.LogEvent/docker-compose.yml
 
 cat /var/lib/jenkins/workspace/test.LogEvent/docker-compose.yml
