cdac36a7bd1b4189062ed55fd09779bf20836e2d
charat <charatkosit@gmail.com>
2023-11-15 22:06:56 +0700

    15-11-66 22:06

--- aws-jenkins/front/dockerPush-front-sit.sh
+++ aws-jenkins/front/dockerPush-front-sit.sh
@@ -8,7 +8,20 @@ docker image push --all-tags charat/go-front-sit
 
 docker images
 docker rmi $(docker images go-front-sit --format "{{.ID}}") -f
-docker rmi -f $(docker images --format "{{.ID}}" --filter "dangling=true")
+
+#Looking for Dangling images
+dangling_images=$(docker images --format "{{.ID}}" --filter "dangling=true")
+
+# There are dangling images ?
+if [ -n "$dangling_images" ]; then
+    # Delete If those are dangling images
+    docker rmi -f $dangling_images
+else
+    # message no action
+    echo "No dangling images"
+fi
+
+
 docker images
 
 curl -X POST -H "Authorization: Bearer ${tokenLineF}" -F "message=SIT push DockerHub  ${releaseFS} OK" https://notify-api.line.me/api/notify
