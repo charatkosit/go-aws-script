54024d8fd5cbd07f079a94c42119ef3722a0a0df
charat <charatkosit@gmail.com>
2023-11-15 17:08:37 +0700

    151166-1708

--- aws-front-sit/script/deploy-front-sit.sh
+++ aws-front-sit/script/deploy-front-sit.sh
@@ -4,7 +4,11 @@ docker rmi $(docker images --format "{{.ID}}" --filter "dangling=true" | head -1
 docker stop $(docker ps --filter "ancestor=charat/go-front-sit:latest" -q)
 docker rm $(docker ps --filter "ancestor=charat/go-front-sit:latest" -aq)
 docker pull charat/go-front-sit:latest
-docker run -p 8000:80 -d charat/go-front-sit:latest
+docker run -p 443:443 \
+-v /home/ec2-user/certbot-certs/live/sit-goplus.ddns.net/fullchain.pem:/etc/ssl/certs/fullchain.pem \
+-v /home/ec2-user/certbot-certs/live/sit-goplus.ddns.net/privkey.pem:/etc/ssl/private/privkey.pem \
+-d charat/go-front-sit:latest
+# docker run -p 8000:80 -d charat/go-front-sit:latest
 curl -X POST -H "Authorization: Bearer ${tokenLineAF}" -F "message=SIT deploy new version http://gosit.ddns.net:8000" https://notify-api.line.me/api/notify
 
 #stat containter when system restart
--- aws-front-uat/script/deploy-front.uat.sh
+++ aws-front-uat/script/deploy-front.uat.sh
@@ -4,7 +4,11 @@ docker rmi $(docker images --format "{{.ID}}" --filter "dangling=true" | head -1
 docker stop $(docker ps --filter "ancestor=charat/go-front-uat:latest" -q)
 docker rm $(docker ps --filter "ancestor=charat/go-front-uat:latest" -aq)
 docker pull charat/go-front-uat:latest
-docker run -p 8000:80 -d charat/go-front-uat:latest
+docker run -p 443:443 \
+-v /home/ec2-user/certbot-certs/live/uat-goplus.ddns.net/fullchain.pem:/etc/ssl/certs/fullchain.pem \
+-v /home/ec2-user/certbot-certs/live/uat-goplus.ddns.net/privkey.pem:/etc/ssl/private/privkey.pem \
+-d charat/go-front-uat:latest
+# docker run -p 8000:80 -d charat/go-front-uat:latest
 curl -X POST -H "Authorization: Bearer ${tokenLineAF}" -F "message=UAT deploy new version http://gouat.ddns.net:8000" https://notify-api.line.me/api/notify
 
 #stat containter when system restart
