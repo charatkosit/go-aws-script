a6d184ddeb73faf2c9d0d679dd7c4b7e56c4355e
charat <charatkosit@gmail.com>
2023-11-16 20:53:22 +0700

    16-11-66 20:53

--- aws-front-uat/script/deploy-front.uat.sh
+++ aws-front-uat/script/deploy-front.uat.sh
@@ -4,7 +4,7 @@ docker rmi $(docker images --format "{{.ID}}" --filter "dangling=true" | head -1
 docker stop $(docker ps --filter "ancestor=charat/go-front-uat:latest" -q)
 docker rm $(docker ps --filter "ancestor=charat/go-front-uat:latest" -aq)
 docker pull charat/go-front-uat:latest
-docker run -p 443:443 \
+docker run -p 80:80 -p 443:443 \
 -v /home/ec2-user/certbot-certs/live/uat-goplus.ddns.net/fullchain.pem:/etc/ssl/certs/fullchain.pem \
 -v /home/ec2-user/certbot-certs/live/uat-goplus.ddns.net/privkey.pem:/etc/ssl/private/privkey.pem \
 -d charat/go-front-uat:latest
