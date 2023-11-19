bc0f62ced8fe474b9ee8e42b9a5317f0b688f212
charat <charatkosit@gmail.com>
2023-11-19 20:10:18 +0700

    19-11-66 20:10

--- aws-log-event/script/deploy-logevent-prod.sh
+++ aws-log-event/script/deploy-logevent-prod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker run -p 3200:3000 -d charat/logevent:latest
+# docker run -p 3200:3000 -d charat/logevent:latest
+docker run -p 3200:3000 -d --restart always charat/logevent:latest
 
 #stat containter when system restart
 docker update --restart=always $(docker ps --format "{{.ID}}")
