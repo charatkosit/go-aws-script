3711e75ccd192cd0589660ec3320c5052cfaaca1
charat <charatkosit@gmail.com>
2023-11-21 12:04:37 +0700

    21-11-66 12:04

---
+++ update-script-epc-prod.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# use for update  
+# 
+# deploy-epc-prod.sh
+git clone https://github.com/charatkosit/go-aws-script.git temp 
+
+
+#update LineToken 
+#AWS-Front:
+tokenLineAF=K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG
+#AWS-Back:
+tokenLineAB=ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz
+
+cat >> ~/.bashrc << EOF
+export tokenLineAF=K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG
+export tokenLineAB=ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz
+EOF
+
+#copy new
+mkdir script
+mkdir Initial
+mkdir docker-mysql-pma
+
+cp /home/ec2-user/temp/aws-epc-prod/script/*.* /home/ec2-user/script/
+cp /home/ec2-user/temp/aws-epc-prod/Initial/*.* /home/ec2-user/Initial/
+cp /home/ec2-user/temp/aws-epc-prod/docker-mysql-pma/*.* /home/ec2-user/docker-mysql-pma/
+
+#chmod
+chmod +x /home/ec2-user/script/*.*
+
+#remove temp
+rm -rf temp
+
+
+
+# ls
+ls -l
+ls -l script
---
+++ update-script-epc-uat.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# use for update  
+# 
+# deploy-epc-uat.sh
+git clone https://github.com/charatkosit/go-aws-script.git temp 
+
+
+#update LineToken 
+#AWS-Front:
+tokenLineAF=K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG
+#AWS-Back:
+tokenLineAB=ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz
+
+cat >> ~/.bashrc << EOF
+export tokenLineAF=K35RgggwSNxmv2UGVT5mGmO5wAwCAuFQuNodqLh5gCG
+export tokenLineAB=ImU3zoEwmB44IwAtpeoqPZihzoLld0xUVeSiy1tD1tz
+EOF
+
+#copy new
+mkdir script
+mkdir Initial
+mkdir docker-mysql-pma
+
+cp /home/ec2-user/temp/aws-epc-uat/script/*.* /home/ec2-user/script/
+cp /home/ec2-user/temp/aws-epc-uat/Initial/*.* /home/ec2-user/Initial/
+cp /home/ec2-user/temp/aws-epc-uat/docker-mysql-pma/*.* /home/ec2-user/docker-mysql-pma/
+
+#chmod
+chmod +x /home/ec2-user/script/*.*
+chmod +x /home/ec2-user/Initial/*.*
+
+
+#remove temp
+rm -rf temp
+
+
+
+# ls
+ls -l
+ls -l script
