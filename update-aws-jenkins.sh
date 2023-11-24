f9a16dfcf32dde4cc696fe8da1247a51e7818272
charat <charatkosit@gmail.com>
2023-11-24 11:44:57 +0700

    24-11-66 11:44

--- update-aws-jenkins.sh
+++ update-aws-jenkins.sh
@@ -12,6 +12,7 @@ cd script
 mkdir front
 mkdir back
 mkdir auth
+mkdir epc
 mkdir click-front
 mkdir click-back
 cd ..
@@ -20,6 +21,7 @@ cd ..
 cp /home/ec2-user/temp/aws-jenkins/front/*.* /home/ec2-user/script/front/
 cp /home/ec2-user/temp/aws-jenkins/back/*.* /home/ec2-user/script/back/
 cp /home/ec2-user/temp/aws-jenkins/auth/*.* /home/ec2-user/script/auth/
+cp /home/ec2-user/temp/aws-jenkins/epc/*.* /home/ec2-user/script/epc/
 cp /home/ec2-user/temp/aws-jenkins/click-front/*.* /home/ec2-user/script/click-front/
 cp /home/ec2-user/temp/aws-jenkins/click-back/*.* /home/ec2-user/script/click-back/
 #cp /home/ec2-user/temp/update-aws-jenkins.sh /home/ec2-user/
@@ -28,6 +30,7 @@ cp /home/ec2-user/temp/aws-jenkins/click-back/*.* /home/ec2-user/script/click-ba
 chmod +x /home/ec2-user/script/front/*.*
 chmod +x /home/ec2-user/script/back/*.*
 chmod +x /home/ec2-user/script/auth/*.*
+chmod +x /home/ec2-user/script/epc/*.*
 chmod +x /home/ec2-user/script/click-front/*.*
 chmod +x /home/ec2-user/script/click-back/*.*
 chmod +x /home/ec2-user/*.*
@@ -41,5 +44,6 @@ ls /home/ec2-user/script/ -la
 ls /home/ec2-user/script/front -la
 ls /home/ec2-user/script/back -la
 ls /home/ec2-user/script/auth -la
+ls /home/ec2-user/script/epc -la
 ls /home/ec2-user/script/click-front -la
 ls /home/ec2-user/script/click-back -la
