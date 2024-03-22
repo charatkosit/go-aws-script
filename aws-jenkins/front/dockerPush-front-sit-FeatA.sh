a04bc2a6637a4b809f7fe8ed1934e182944092b5
charatkosit <charatkosit@gmail.com>
2024-03-22 14:34:27 +0700

    2203671434

---
+++ aws-jenkins/front/dockerPush-front-sit-FeatA.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+releaseFS=`docker images go-front-sit-FeatA --format {{.Tag}} | head -1`
+
+docker image tag go-front-sit-FeatA:$releaseFS go-front-sit-FeatA:latest
+docker image tag go-front-sit-FeatA:$releaseFS charat/go-front-sit-FeatA:$releaseFS 
+docker image tag go-front-sit-FeatA:latest charat/go-front-sit-FeatA:latest
+docker image push --all-tags charat/go-front-sit-FeatA
+
+docker images
+docker rmi $(docker images go-front-sit-FeatA --format "{{.ID}}") -f
+
+#Looking for Dangling images
+dangling_images=$(docker images --format "{{.ID}}" --filter "dangling=true")
+
+# There are dangling images ?
+if [ -n "$dangling_images" ]; then
+    # Delete If those are dangling images
+    docker rmi -f $dangling_images
+else
+    # message no action
+    echo "No dangling images"
+fi
+
+
+docker images
+
+curl -X POST -H "Authorization: Bearer ${tokenLineF}" -F "message=SIT-FeatA push DockerHub  ${releaseFS} OK" https://notify-api.line.me/api/notify
