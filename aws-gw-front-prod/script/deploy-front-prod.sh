6c7d2c07726eede02907e1219f91526654a07307
charatkosit <charatkosit@gmail.com>
2024-06-06 15:40:37 +0700

    25670606-1540

--- aws-gw-front-prod/script/deploy-front-prod.sh
+++ aws-gw-front-prod/script/deploy-front-prod.sh
@@ -1,15 +1,15 @@
 #!bin/bash
 docker stop $(docker ps --format "{{.ID}}")
 docker rmi $(docker images --format "{{.ID}}" --filter "dangling=true" | head -1)
-docker stop $(docker ps --filter "ancestor=charat/go-front-prod:latest" -q)
-docker rm $(docker ps --filter "ancestor=charat/go-front-prod:latest" -aq)
-docker pull charat/go-front-prod:latest
+docker stop $(docker ps --filter "ancestor=charat/gw-front-prod:latest" -q)
+docker rm $(docker ps --filter "ancestor=charat/gw-front-prod:latest" -aq)
+docker pull charat/gw-front-prod:latest
 docker run -p 80:80 -p 443:443 \
--v /home/ec2-user/certbot-certs/live/go.gpautoparts.co.th/fullchain.pem:/etc/ssl/certs/fullchain.pem \
--v /home/ec2-user/certbot-certs/live/go.gpautoparts.co.th/privkey.pem:/etc/ssl/private/privkey.pem \
--d charat/go-front-prod:latest
-# docker run -p 8000:80 -d charat/go-front-prod:latest
-curl -X POST -H "Authorization: Bearer ${tokenLineAF}" -F "message=PROD deploy new version https://go.gpautoparts.co.th" https://notify-api.line.me/api/notify
+-v /home/ec2-user/certbot-certs/live/gw-goplus.ddns.net/fullchain.pem:/etc/ssl/certs/fullchain.pem \
+-v /home/ec2-user/certbot-certs/live/gw-goplus.ddns.net/privkey.pem:/etc/ssl/private/privkey.pem \
+-d charat/gw-front-prod:latest
+# docker run -p 8000:80 -d charat/gw-front-prod:latest
+curl -X POST -H "Authorization: Bearer ${tokenLineAF}" -F "message=PROD deploy new version https://gw-goplus.ddns.net" https://notify-api.line.me/api/notify
 
 #stat containter when system restart
 docker update --restart=always $(docker ps --format "{{.ID}}")
