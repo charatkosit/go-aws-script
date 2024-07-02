41c3def3c82720c5c602c76b162c8414004e486c
charatkosit <charatkosit@gmail.com>
2024-07-02 19:30:48 +0700

    2024-08-02 19:30

--- new-install-jenkins-centos.sh
+++ new-install-jenkins-centos.sh
@@ -5,7 +5,7 @@ ln -sf /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
 date
 
 #install docker
-yum update -y
+dnf update -y
 yum install docker -y
 service docker start
 systemctl enable --now docker
@@ -38,14 +38,27 @@ export tokenLineDeployGroup=nS2OKcPZ9lXNb03JHJ7xzl4YNDBdzswkTg1qK8iNDQD
 EOF
 
 #jenkins
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum upgrade
+dnf install java-11-openjdk -y
 
-yum install jenkins -y
+sudo tee /etc/yum.repos.d/jenkins.repo<<EOF
+[jenkins]
+name=Jenkins-stable
+baseurl=https://pkg.jenkins.io/redhat-stable/
+gpgcheck=1
+EOF
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+
+
+dnf install jenkins -y
 systemctl enable jenkins
 systemctl start jenkins
+systemctl status jenkins
+
 jenkinsPass=`cat /var/lib/jenkins/secrets/initialAdminPassword`
+
+#firewall
+firewall-cmd --permanent --zone=public --add-port=8080/tcp
+firewall-cmd --reload
 
 #portainer
 # mkdir /opt/portainer_data
